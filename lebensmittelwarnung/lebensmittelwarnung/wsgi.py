51f152e6929d732714fdfb0bf466c3e7557be63d
Arie Arya <ariearya@gmail.com>
2023-01-17 22:32:28 +0000

    use whitenoise for serving static data

--- lebensmittelwarnung/lebensmittelwarnung/wsgi.py
+++ lebensmittelwarnung/lebensmittelwarnung/wsgi.py
@@ -8,9 +8,10 @@ https://docs.djangoproject.com/en/4.1/howto/deployment/wsgi/
 """
 
 import os
-
+from whitenoise.django import DjangoWhiteNoise
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'lebensmittelwarnung.settings')
 
 application = get_wsgi_application()
+application = DjangoWhiteNoise(application)
